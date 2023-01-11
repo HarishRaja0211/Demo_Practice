ded4273191d9ee73745f1f8c65ba9bfa0c3cfec8
Harish_Raja0211 <hayalishi0211@gmail.com>
2023-01-11 13:41:16 +0530

    cleared an bug

--- user_calculator.py
+++ user_calculator.py
@@ -1,4 +1,13 @@
-no_1 = int(input("enter the first number:-"));
-no_2 = int(input("enter the second number -"));
+while(True):
+    try:
+        
+        no_1 = int(input("enter the first number:-"))
+        no_2 = int(input("enter the second number -"))
+ 
+        print("addition of two numbers: -",no_1+no_2)
 
-print("addition of two numbers: -",no_1+no_2);
+        break
+
+    except:
+        print("enter the int")
+
